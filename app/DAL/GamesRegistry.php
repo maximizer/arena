9396606b8160e099755a5084168b57421ea29dc8
Patrick Mizer <patrick@sparefoot.com>
2018-08-10 13:26:24 -0500

    added pdo objects

--- app/DAL/GamesRegistry.php
+++ app/DAL/GamesRegistry.php
@@ -8,11 +8,13 @@
 
 namespace App\DAL;
 
-
 use App\Model\GameModel;
 
 class GamesRegistry extends DAL
 {
+    /**
+     * @return array
+     */
     public function selectAllGames() {
 
         $pdo = $this->getPdo();
@@ -29,15 +31,53 @@ class GamesRegistry extends DAL
     }
 
     /**
-     * @param $row
-     * @return GameModel
+     * @param $id
      */
+    public function selectGameById($id) {
+        $pdo = $this->getPdo();
+
+        $stmt = $pdo->prepare("SELECT * FROM games WHERE game_id = :game_id");
+        $stmt->execute([':game_id' => $id]);
+
+        $obj = $stmt->fetchObject();
+    }
+
+    /**
+     * @param GameModel $game
+     */
+    public function updateGame(GameModel $game) {
+        $pdo = $this->getPdo();
+
+        $stmt = $pdo->prepare("UPDATE games SET name = :name WHERE game_id = :game_id");
+        $stmt->execute([':id' => $game->id, ':game_id' => $game->id]);
+    }
+
+    /**
+     * @param $id
+     */
+    public function deleteGame($id) {
+        $pdo = $this->getPdo();
+
+        $stmt = $pdo->prepare("DELETE FROM games WHERE game_id = :game_id");
+        $stmt->execute([':game_id' => $id]);
+    }
+
+    /**
+     * @param GameModel $game
+     */
+    public function insertGame(GameModel $game) {
+        $pdo = $this->getPdo();
+
+        $stmt = $pdo->prepare("INSERT INTO games (name) VALUES (:name)");
+        $stmt->execute([':name' => $game->name]);
+    }
+
     private function _mapRowToModel($row) {
         $game = new GameModel();
-
         $game->id = $row->game_id;
         $game->name = $row->name;
 
         return $game;
     }
+
 }
